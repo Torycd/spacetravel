10510ecbc6bd01936284e2bf9412984d76fd2d64
Torycd <vicidundun@gmail.com>
2024-02-03 17:18:41 +0100

    Lets Do this

--- src/Pages/Destination.jsx
+++ src/Pages/Destination.jsx
@@ -1,12 +1,25 @@
-import { NavLink, Outlet } from "react-router-dom";
+import { NavLink, Outlet, useLocation } from "react-router-dom";
 import moon from "../assets/image-moon.png";
+// import titan from "../assets/image-titan.png";
+// import mars from "../assets/image-mars.png"
+// import europa from "../assets/image-europa.png"
 
 const Destination = () => {
+  const location = useLocation()
+
+  let img = moon;
+  if (location.pathname == "/" ){
+    // work on the decision
+    return;
+  } else if ( location.pathname == "//" ){
+    return;
+  }
+
 
   return (
     <div className="text-white grid sm:grid-cols-2">
       <div>
-        <img className="ml-10" src={moon} alt="#" />
+        <img className="ml-10" src={img} alt="#" />
       </div>
 
       <div>
@@ -17,21 +30,21 @@ const Destination = () => {
                 to="/Destination"
                 className={({ isActive }) =>
                   isActive
-                    ? "h-full flex items-center gap-2 border-b-4 border-white"
-                    : "h-full flex items-center gap-2 border-none"
+                    ? "h-full flex items-center gap-2 border-b-4 border-white pb-2"
+                    : "h-full flex items-center gap-2 border-none pb-2"
                 }
                 end
               >
                 MOON
               </NavLink>
             </li>
-            <li className="h-full flex items-center">
+            <li className="h-full flex items-center pb-2">
               <NavLink
                 to="/Destination/Mars"
                 className={({ isActive }) =>
                   isActive
-                    ? "h-full flex items-center gap-2 border-b-4 border-white"
-                    : "h-full flex items-center gap-2 border-none"
+                    ? "h-full flex items-center gap-2 border-b-4 border-white pb-2"
+                    : "h-full flex items-center gap-2 border-none pb-2"
                 }
               >
                 MARS
@@ -42,8 +55,8 @@ const Destination = () => {
                 to="/Destination/Europa"
                 className={({ isActive }) =>
                   isActive
-                    ? "h-full flex items-center gap-2 border-b-4 border-white"
-                    : "h-full flex items-center gap-2 border-none"
+                    ? "h-full flex items-center gap-2 border-b-4 border-white pb-2"
+                    : "h-full flex items-center gap-2 border-none pb-2"
                 }
               >
                 EUROPA
@@ -54,8 +67,8 @@ const Destination = () => {
                 to="/Destination/Titan"
                 className={({ isActive }) =>
                   isActive
-                    ? "h-full flex items-center gap-2 border-b-4 border-white"
-                    : "h-full flex items-center gap-2 border-none"
+                    ? "h-full flex items-center gap-2 border-b-4 border-white pb-2"
+                    : "h-full flex items-center gap-2 border-none pb-2"
                 }
               >
                 TITAN
--- src/components/Titan.jsx
+++ src/components/Titan.jsx
@@ -3,7 +3,9 @@ const Titan = () => {
     <div className="flex flex-col gap-10 text-left mt-20">
       <h1 className="text-8xl">Titan</h1>
       <p>
-      Titan
+        The only moon known to have a dense atmosphere other than Earth, Titan
+        is a home away from home (just a few hundred degrees colder!). As a
+        bonus, you get striking views of the Rings of Saturn.
       </p>
       <hr className="bg-white mr-40" />
       <div className="flex justify-around mr-40">
