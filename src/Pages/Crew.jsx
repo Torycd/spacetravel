2cd2dfbe573c4a97f4353e96805abb2b2dc9e889
Torycd <vicidundun@gmail.com>
2024-02-06 09:59:14 +0100

    lets take things to the next level

--- src/Pages/Crew.jsx
+++ src/Pages/Crew.jsx
@@ -1,8 +1,28 @@
+// import { Carousel, Typography } from "@material-tailwind/react"
+// import { CarouselDefault } from "../components/UI/CarouselCustomNavigation"
+import { CarouselCustomNavigation } from "../components/UI/Carousel"
+// import crew1 from "../assets/image-anousheh-ansari.png";
+// import crew2 from "../assets/image-douglas-hurley.png";
+// import crew3 from "../assets/image-victor-glover.png"
 
 
 const Crew = () => {
   return (
-    <div>Crew</div>
+    // <Carousel>
+    //   <div className="relative h-full w-full">
+    //     <Typography>Crew!</Typography>
+    //     <img src={crew1} className="h-full w-full object-cover" alt="/"/>
+    //   </div>
+    //   <div className="relative h-full w-full">
+    //     <Typography>Crew2</Typography>
+    //     <img src={crew2} alt="/"/>
+    //   </div>
+    //   <div className="relative h-full w-full">
+    //     <Typography>Crew3</Typography>
+    //     <img src={crew3} alt="/"/>
+    //   </div>
+    // </Carousel>
+    <CarouselCustomNavigation/>
   )
 }
 
---
+++ src/components/UI/Carousel.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+
+import crew1 from "../../assets//image-anousheh-ansari.png";
+import crew2 from "../../assets/image-douglas-hurley.png";
+import crew3 from "../../assets/image-victor-glover.png";
+
+const Carousel = () => {
+  const slides = [
+    {
+      img: crew1,
+      postion: "Commander",
+      name: "Douglas Hurley",
+      about:
+        "Douglas Gerald Hurley is an American engineer, former Marine Corps pilot and former NASA astronaut. He launched into space for the third time as commander of Crew Dragon Demo2.",
+    },
+    {
+      img: crew2,
+      postion: "Flight Engineer",
+      name: "Anousheh Ansari",
+      about:
+        "Anousheh Ansari is an Iranian American engineer and co-founder of Prodea Systems. Ansari was the fourth self-funded space tourist, the first self-funded woman to fly to the ISS, and the first Iranian in space.",
+    },
+    {
+      img: crew3,
+      postion: "Pilot",
+      name: "Victor Glover",
+      about:
+        " Pilot on the first operational flight of the SpaceX Crew Dragon to the International Space Station. Glover is a commander in the U.S. Navy where he pilots an F/A-18.He was a crew member of Expedition 64, and served as a station systems flight engineer. ",
+    },
+  ];
+
+  return (
+    <div className="max-w-full h-full grid  m-auto">
+      <div style={{backgroundImage: slides[0].img }} className="w-full h-full duration-500"></div>
+    </div>
+  );
+};
+
+export default Carousel;
