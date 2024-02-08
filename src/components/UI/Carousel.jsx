88e9a60093f253414abded7b617a009f27d3427a
Torycd <vicidundun@gmail.com>
2024-02-08 23:45:11 +0100

    ggg

--- src/components/UI/Carousel.jsx
+++ src/components/UI/Carousel.jsx
@@ -5,13 +5,13 @@ const Carousel = ({ slides }) => {
     <div className="overflow-hidden">
       <div className="flex justify-center">
         {slides.map((s) => (
-          <div key={s.name} className="flex w-[100%]">
-            <div className="flex">
+          <div key={s.name} className="flex w-full">
+            <div className="flex w-1/2">
               <h1>{s.name}</h1>
-              <h1>{s.postion}</h1>
+              <h1>{s.position}</h1>
               <p>{s.about}</p>
             </div>
-            <img src={s.img} />
+            <img src={s.img} className="w-1/2" alt={s.name} />
           </div>
         ))}
       </div>
