b44ec3aab4c64cd9e2e6e5e3d46525fea629beeb
Torycd <vicidundun@gmail.com>
2024-02-08 23:08:03 +0100

    yyy

--- src/Pages/Crew.jsx
+++ src/Pages/Crew.jsx
@@ -1,29 +1,38 @@
-// import { Carousel, Typography } from "@material-tailwind/react"
-// import { CarouselDefault } from "../components/UI/CarouselCustomNavigation"
 import { CarouselCustomNavigation } from "../components/UI/Carousel"
-// import crew1 from "../assets/image-anousheh-ansari.png";
-// import crew2 from "../assets/image-douglas-hurley.png";
-// import crew3 from "../assets/image-victor-glover.png"
+import crew1 from "../assets/image-anousheh-ansari.png";
+import crew2 from "../assets/image-douglas-hurley.png";
+import crew3 from "../assets/image-victor-glover.png"
+
+
 
 
 const Crew = () => {
+    const slides = [
+      {
+        img: crew1,
+        postion: "Commander",
+        name: "Douglas Hurley",
+        about:
+          "Douglas Gerald Hurley is an American engineer, former Marine Corps pilot and former NASA astronaut. He launched into space for the third time as commander of Crew Dragon Demo2.",
+      },
+      {
+        img: crew2,
+        postion: "Flight Engineer",
+        name: "Anousheh Ansari",
+        about:
+          "Anousheh Ansari is an Iranian American engineer and co-founder of Prodea Systems. Ansari was the fourth self-funded space tourist, the first self-funded woman to fly to the ISS, and the first Iranian in space.",
+      },
+      {
+        img: crew3,
+        postion: "Pilot",
+        name: "Victor Glover",
+        about:
+          " Pilot on the first operational flight of the SpaceX Crew Dragon to the International Space Station. Glover is a commander in the U.S. Navy where he pilots an F/A-18.He was a crew member of Expedition 64, and served as a station systems flight engineer. ",
+      },
+    ];
   return (
-    // <Carousel>
-    //   <div className="relative h-full w-full">
-    //     <Typography>Crew!</Typography>
-    //     <img src={crew1} className="h-full w-full object-cover" alt="/"/>
-    //   </div>
-    //   <div className="relative h-full w-full">
-    //     <Typography>Crew2</Typography>
-    //     <img src={crew2} alt="/"/>
-    //   </div>
-    //   <div className="relative h-full w-full">
-    //     <Typography>Crew3</Typography>
-    //     <img src={crew3} alt="/"/>
-    //   </div>
-    // </Carousel>
-    <CarouselCustomNavigation/>
+    <CarouselCustomNavigation slides={slides}/>
   )
 }
 
-export default Crew
+export default Crew;
