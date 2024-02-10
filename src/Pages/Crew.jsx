67a1fe25363732fccea147ae01b619a759330a91
Torycd <vicidundun@gmail.com>
2024-02-10 21:43:22 +0100

    lets cook

--- src/Pages/Crew.jsx
+++ src/Pages/Crew.jsx
@@ -10,21 +10,21 @@ const Crew = () => {
     const slides = [
       {
         img: crew1,
-        postion: "Commander",
+        position: "Commander",
         name: "Douglas Hurley",
         about:
           "Douglas Gerald Hurley is an American engineer, former Marine Corps pilot and former NASA astronaut. He launched into space for the third time as commander of Crew Dragon Demo2.",
       },
       {
         img: crew2,
-        postion: "Flight Engineer",
+        position: "Flight Engineer",
         name: "Anousheh Ansari",
         about:
           "Anousheh Ansari is an Iranian American engineer and co-founder of Prodea Systems. Ansari was the fourth self-funded space tourist, the first self-funded woman to fly to the ISS, and the first Iranian in space.",
       },
       {
         img: crew3,
-        postion: "Pilot",
+        position: "Pilot",
         name: "Victor Glover",
         about:
           " Pilot on the first operational flight of the SpaceX Crew Dragon to the International Space Station. Glover is a commander in the U.S. Navy where he pilots an F/A-18.He was a crew member of Expedition 64, and served as a station systems flight engineer. ",
