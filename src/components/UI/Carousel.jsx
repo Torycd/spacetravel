87ff4907de44935717e658f1e06584205bf492ad
Torycd <vicidundun@gmail.com>
2024-02-10 21:42:20 +0100

    yy

--- src/components/UI/Carousel.jsx
+++ src/components/UI/Carousel.jsx
@@ -3,24 +3,24 @@ import PropTypes from "prop-types";
 const Carousel = ({ slides }) => {
   return (
     <div className="overflow-hidden">
-      <div className="flex justify-center">
-        {slides.map((s) => (
-          <div key={s.name} className="flex w-full">
-            <div className="flex w-1/2">
+      {slides.map((s) => (
+        <div key={s.name} className="flex justify-center">
+          <div className="flex w-full">
+            <div className="w-1/2">
               <h1>{s.name}</h1>
               <h1>{s.position}</h1>
               <p>{s.about}</p>
             </div>
             <img src={s.img} className="w-1/2" alt={s.name} />
           </div>
-        ))}
-      </div>
+        </div>
+      ))}
     </div>
   );
 };
 
 Carousel.propTypes = {
-  slides: PropTypes.node.isRequired,
+  slides: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
 
 export default Carousel;
