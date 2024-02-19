e3047f66af68274f60305eae803073e48e6c376a
Torycd <vicidundun@gmail.com>
2024-02-19 22:13:21 +0100

    lets freaking go

--- src/Pages/Technology.jsx
+++ src/Pages/Technology.jsx
@@ -27,49 +27,55 @@ const Technology = () => {
         <h2 className="font-Inte text-2xl opacity-90">Space Launch 101</h2>
       </div>
 
-      <div className="flex justify-between">
-        <div className="flex gap-10">
+      <div className="flex flex-col-reverse lg:flex-row justify-between">
+        <div className="flex flex-col lg:flex-row gap-10">
           {/* rounded item */}
-          <div className="text-black font-Inte ml-32 my-auto flex flex-col gap-5 font-bold">
+          <div className="text-black font-Inte mx-auto lg:ml-32 mt-4 lg:my-auto flex lg:flex-col gap-5 font-bold">
             <NavLink
               to="/Technology"
               className={({ isActive }) =>
                 isActive
-                  ? "bg-white w-24 h-24 rounded-full flex"
-                  : "border-2 w-24 h-24 rounded-full flex border-slate-200 bg-slate-800 text-white"
+                  ? "bg-white w-12 h-12 md:w-24 md:h-24 rounded-full flex"
+                  : "border-2 w-12 h-12 md:w-24 md:h-24 rounded-full flex border-slate-200 bg-slate-800 text-white"
               }
               end
             >
-              <span className="self-center mx-auto text-4xl">1</span>
+              <span className="self-center mx-auto text-2xl md:text-4xl">
+                1
+              </span>
             </NavLink>
             <NavLink
               to="/Technology/Capsule"
               className={({ isActive }) =>
                 isActive
-                  ? "bg-white w-24 h-24 rounded-full flex"
-                  : "border-2 w-24 h-24 rounded-full flex border-slate-200 bg-slate-800 text-white"
+                  ? "bg-white w-12 h-12 md:w-24 md:h-24 rounded-full flex"
+                  : "border-2 w-12 h-12 md:w-24 md:h-24 rounded-full flex border-slate-200 bg-slate-800 text-white"
               }
             >
-              <span className="self-center mx-auto text-4xl">2</span>
+              <span className="self-center mx-auto text-2xl md:text-4xl">
+                2
+              </span>
             </NavLink>
             <NavLink
               to="/Technology/Vehicle"
               className={({ isActive }) =>
                 isActive
-                  ? "bg-white w-24 h-24 rounded-full flex"
-                  : "border-2 w-24 h-24 rounded-full flex border-slate-200 bg-slate-800 text-white"
+                  ? "bg-white w-12 h-12 md:w-24 md:h-24 rounded-full flex"
+                  : "border-2 w-12 h-12 md:w-24 md:h-24 rounded-full flex border-slate-200 bg-slate-800 text-white"
               }
             >
-              <span className="self-center mx-auto text-4xl">3</span>
+              <span className="self-center mx-auto text-2xl md:text-4xl">
+                3
+              </span>
             </NavLink>
           </div>
           {/* information */}
-          <div className="my-auto flex flex-col gap-8 ">
-            <h1 className="text-2xl font-bold font-Inte opacity-70">
+          <div className="my-auto flex flex-col gap-2 lg:gap-8 ">
+            <h1 className="text-xl md:text-2xl font-bold font-Inte opacity-70">
               The terminology...
             </h1>
-            <h2 className="text-6xl font-semibold font-Inte">{name}</h2>
-            <p className="right opacity-70">{statement}</p>
+            <h2 className="text-4xl md:text-6xl font-semibold font-Inte">{name}</h2>
+            <p className="text-center lg:right opacity-70">{statement}</p>
           </div>
         </div>
         <Outlet />
--- src/components/Technology/Capsule.jsx
+++ src/components/Technology/Capsule.jsx
@@ -1,9 +1,13 @@
 import Tech1 from "../../assets/image-space-capsule-portrait.jpg";
+import Tech2 from "../../assets/image-space-capsule-landscape.jpg";
 
 const Capsule = () => {
   return (
-    <img src={Tech1} />
-  )
-}
+    <>
+      <img className="hidden md:block" src={Tech1} />
+      <img className="md:hidden" src={Tech2} />
+    </>
+  );
+};
 
-export default Capsule
+export default Capsule;
