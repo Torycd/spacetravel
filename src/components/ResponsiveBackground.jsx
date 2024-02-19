4424f84500a8e4778834dfedaf31ed846a84fdb7
Torycd <vicidundun@gmail.com>
2024-02-19 22:45:48 +0100

    now remianing Navigation

--- src/components/ResponsiveBackground.jsx
+++ src/components/ResponsiveBackground.jsx
@@ -4,6 +4,15 @@ import { useLocation } from "react-router-dom";
 import desktopImage from "../assets/background-home-desktop.jpg";
 import tabletImage from "../assets/background-home-tablet.jpg";
 import mobileImage from "../assets/background-home-mobile.jpg";
+import destinationDesktopImage from "../assets/background-destination-desktop.jpg";
+import destinationTabletImage from "../assets/background-destination-tablet.jpg";
+import destinationMobileImage from "../assets/background-destination-mobile.jpg";
+import crewDesktopImage from "../assets/background-crew-desktop.jpg";
+import crewTabletImage from "../assets/background-crew-tablet.jpg";
+import crewMobileImage from "../assets/background-crew-mobile.jpg";
+import technologyDesktopImage from "../assets/background-technology-desktop.jpg";
+import technologyTabletImage from "../assets/background-technology-tablet.jpg";
+import technologyMobileImage from "../assets/background-technology-mobile.jpg";
 
 const ResponsiveBackground = ({ children }) => {
   const location = useLocation();
@@ -16,30 +25,29 @@ const ResponsiveBackground = ({ children }) => {
     backgroundImage = desktopImage;
     backgroundImageTablet = tabletImage;
     backgroundImageMobile = mobileImage;
-  } 
-  // else if (
-  //   location.pathname === "/Destination" ||
-  //   location.pathname === "/Destination/Mars" ||
-  //   location.pathname === "/Destination/Moon" ||
-  //   location.pathname === "/Destination/Titan" ||
-  //   location.pathname === "/Destination/Europa"
-  // ) {
-  //   backgroundImage = destinationDesktopImage;
-  //   backgroundImageTablet = destinationTabletImage;
-  //   backgroundImageMobile = destinationMobileImage;
-  // } else if (location.pathname === "/Crew") {
-  //   backgroundImage = crewDesktopImage;
-  //   backgroundImageTablet = crewTabletImage;
-  //   backgroundImageMobile = crewMobileImage;
-  // } else if (
-  //   location.pathname === "/Technology" ||
-  //   location.pathname === "/Technology/Capsule" ||
-  //   location.pathname === "/Technology/Vehicle"
-  // ) {
-  //   backgroundImage = technologyDesktopImage;
-  //   backgroundImageTablet = technologyTabletImage;
-  //   backgroundImageMobile = technologyMobileImage;
-  // }
+  } else if (
+    location.pathname === "/Destination" ||
+    location.pathname === "/Destination/Mars" ||
+    location.pathname === "/Destination/Moon" ||
+    location.pathname === "/Destination/Titan" ||
+    location.pathname === "/Destination/Europa"
+  ) {
+    backgroundImage = destinationDesktopImage;
+    backgroundImageTablet = destinationTabletImage;
+    backgroundImageMobile = destinationMobileImage;
+  } else if (location.pathname === "/Crew") {
+    backgroundImage = crewDesktopImage;
+    backgroundImageTablet = crewTabletImage;
+    backgroundImageMobile = crewMobileImage;
+  } else if (
+    location.pathname === "/Technology" ||
+    location.pathname === "/Technology/Capsule" ||
+    location.pathname === "/Technology/Vehicle"
+  ) {
+    backgroundImage = technologyDesktopImage;
+    backgroundImageTablet = technologyTabletImage;
+    backgroundImageMobile = technologyMobileImage;
+  }
 
   return (
     <>
